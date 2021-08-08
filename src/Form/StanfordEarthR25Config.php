b71df8c9f9beaf863a66e14aadc9f41b007a0b27
Kassandra Sharp <ksharp@stanford.edu>
2021-08-08 16:19:46 -0700

    EARTH-0000 Early check-in. Nothing to see here yet.

--- src/Form/StanfordEarthR25Config.php
+++ src/Form/StanfordEarthR25Config.php
@@ -1,17 +1,18 @@
 <?php
 
-namespace Drupal\stanford_earth_workgroups\Form;
+namespace Drupal\stanford_earth_r25\Form;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\stanford_earth_workgroups\Service\StanfordEarthWorkgroupsService;
+use Drupal\stanford_earth_r25\Service\StanfordEarthR25Service;
+use Symfony\Component\Yaml\Yaml;
 
 /**
- * Contains Drupal\stanford_earth_workgroups\Form\StanfordEarthWorkgroupsConfig.
+ * Contains Drupal\stanford_earth_r25\Form\StanfordEarthR25Config.
  */
-class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
+class StanfordEarthR25Config extends ConfigFormBase {
 
   /**
    * The configuration factory.
@@ -21,24 +22,24 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
   protected $configFactory;
 
   /**
-   * The workgroup service.
+   * The R25 service.
    *
-   * @var \Drupal\stanford_earth_workgroups\Service\StanfordEarthWorkgroupsService
+   * @var \Drupal\stanford_earth_r25\Service\StanfordEarthR25Service
    */
-  protected $wgService;
+  protected $r25Service;
 
   /**
-   * StanfordEarthWorkgroupsConfig constructor.
+   * StanfordEarthR25Config constructor.
    *
    * @param \Drupal\Core\Config\ConfigFactoryInterface $configFactory
    *   The ConfigFactory interface.
-   * @param \Drupal\stanford_earth_workgroups\Service\StanfordEarthWorkgroupsService $wgService
+   * @param \Drupal\stanford_earth_r25\Service\StanfordEarthR25Service $r25Service
    *   The Workgroup service.
    */
   public function __construct(ConfigFactoryInterface $configFactory,
-    StanfordEarthWorkgroupsService $wgService) {
+    StanfordEarthR25Service $r25Service) {
     $this->configFactory = $configFactory;
-    $this->wgService = $wgService;
+    $this->r25Service = $r25Service;
     parent::__construct($configFactory);
   }
 
@@ -48,7 +49,7 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('config.factory'),
-      $container->get('stanford_earth_workgroups.workgroup')
+      $container->get('stanford_earth_r25.r25_call')
     );
   }
 
@@ -57,7 +58,7 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return [
-      'stanford_earth_workgroups.adminsettings',
+      'stanford_earth_r25.adminsettings',
     ];
   }
 
@@ -65,39 +66,70 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
    * {@inheritdoc}
    */
   public function getFormId() {
-    return 'stanford_earth_workgroups_config';
+    return 'stanford_earth_r25_config';
   }
 
   /**
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $config = $this->config('stanford_earth_workgroups.adminsettings');
-
-    $form['stanford_earth_workgroups_cert'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('MAIS Certificate Path'),
-      '#description' => $this->t('Location on server of the MAIS WG API cert.'),
-      '#required' => TRUE,
-      '#default_value' => $config->get('stanford_earth_workgroups_cert'),
+    $rooms = [
+      'langcode' => 'en',
+      'status' => 'true',
+      'dependencies' => [],
+      'id' => 'stanford_earth_r25_rooms',
+      'label' => 'Stanford Earth R25 Rooms',
+      'description' => 'Stanford Earth R25 Room Configurations',
+      'source_type' => null,
+      'module' => null,
     ];
 
-    $form['stanford_earth_workgroups_key'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('MAIS Key Path'),
-      '#description' => $this->t('Location on server of the MAIS WG API key.'),
-      '#required' => TRUE,
-      '#default_value' => $config->get('stanford_earth_workgroups_key'),
-    ];
+    $db = \Drupal::database();
+    $result = $db->query("SELECT value FROM {room_test} WHERE name = :sunetid", [':sunetid' => 'r25_room']);
+    foreach ($result as $record) {
+      $rooms['rooms'] = unserialize($record->value);
+    }
 
-    $form['stanford_earth_workgroups_test'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Test Workgroup'),
-      '#description' => $this->t('A Stanford Workgroup to test your cert and key.'),
-      '#required' => TRUE,
-      '#default_value' => $config->get('stanford_earth_workgroups_test'),
-    ];
+    //$dumper = new Yaml();
+    //$room_yaml = $dumper->dump($rooms);
+    $room_yaml = Yaml::dump($rooms,10,2);
 
+    $config = $this->config('stanford_earth_r25.adminsettings');
+    $r25_service = \Drupal::service('stanford_earth_r25.r25_call');
+    //$r25_result = $r25_service->r25_api_call();
+    $args = 'space_id=1675&scope=extended&start_dt=20210801T00000000&end_dt=20210901T00000000';
+    $items = array();
+    // make the API call
+    $r25_result = $r25_service->r25_api_call('feed', $args);
+
+    $xyz = 1;
+
+      /*
+          $form['stanford_earth_workgroups_cert'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('MAIS Certificate Path'),
+            '#description' => $this->t('Location on server of the MAIS WG API cert.'),
+            '#required' => TRUE,
+            '#default_value' => $config->get('stanford_earth_workgroups_cert'),
+          ];
+
+          $form['stanford_earth_workgroups_key'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('MAIS Key Path'),
+            '#description' => $this->t('Location on server of the MAIS WG API key.'),
+            '#required' => TRUE,
+            '#default_value' => $config->get('stanford_earth_workgroups_key'),
+          ];
+
+          $form['stanford_earth_workgroups_test'] = [
+            '#type' => 'textfield',
+            '#title' => $this->t('Test Workgroup'),
+            '#description' => $this->t('A Stanford Workgroup to test your cert and key.'),
+            '#required' => TRUE,
+            '#default_value' => $config->get('stanford_earth_workgroups_test'),
+          ];
+
+      */
     return parent::buildForm($form, $form_state);
   }
 
@@ -106,6 +138,7 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
    */
   public function validateForm(array &$form, FormStateInterface $form_state) {
     parent::validateForm($form, $form_state);
+/*
     $wg = $form_state->getValue('stanford_earth_workgroups_test');
     $wg_data = $this->wgService->getMembers($wg,
       $form_state->getValue('stanford_earth_workgroups_cert'),
@@ -114,6 +147,7 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
       $form_state->setErrorByName('stanford_earth_workgroups_test',
         $wg_data['status']['message']);
     }
+*/
   }
 
   /**
@@ -121,11 +155,13 @@ class StanfordEarthWorkgroupsConfig extends ConfigFormBase {
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     parent::submitForm($form, $form_state);
+/*
     $this->configFactory->getEditable('stanford_earth_workgroups.adminsettings')
       ->set('stanford_earth_workgroups_test', $form_state->getValue('stanford_earth_workgroups_test'))
       ->set('stanford_earth_workgroups_cert', $form_state->getValue('stanford_earth_workgroups_cert'))
       ->set('stanford_earth_workgroups_key', $form_state->getValue('stanford_earth_workgroups_key'))
       ->save();
+*/
   }
 
 }
---
+++ src/Service/StanfordEarthR25Service.php
@@ -0,0 +1,273 @@
+<?php
+
+namespace Drupal\stanford_earth_r25\Service;
+
+use Drupal\Core\Config\ConfigFactory;
+use Drupal\Core\Logger\LoggerChannelFactoryInterface;
+use Drupal\Core\Mail\MailManager;
+use Drupal\Core\Session\AccountProxy;
+use GuzzleHttp\ClientInterface;
+use Drupal\migrate_plus\Plugin\migrate_plus\data_parser\SimpleXml;
+
+/**
+ * Service to interface with CollegeNet R25 (25Live) API.
+ */
+class StanfordEarthR25Service {
+
+  /**
+   * Global config object.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $config;
+
+  protected $httpClient;
+
+  /**
+   * The logger service.
+   *
+   * @var \Drupal\Core\Logger\LoggerChannelInterface
+   */
+  protected $logger;
+
+  /**
+   * The Mail Manager service.
+   *
+   * @var \Drupal\Core\Mail\MailManager
+   */
+  protected $mailManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxy
+   */
+  protected $currentUser;
+
+  /**
+   * Global site settings.
+   *
+   * @var \Drupal\Core\Config\ImmutableConfig
+   */
+  protected $settings;
+
+  /**
+   * Constructs a StanfordEarthR25Service object.
+   *
+   * @param \GuzzleHttp\ClientInterface $http_client
+   *   The Guzzle HTTP client.
+   * @param \Drupal\Core\Config\ConfigFactory $config
+   *   Drupal config data.
+   * @param \Drupal\Core\Logger\LoggerChannelFactoryInterface $logger_factory
+   *   The logger factory service.
+   * @param \Drupal\Core\Mail\MailManager $mailmgr
+   *   The mail manager service.
+   * @param \Drupal\Core\Session\AccountProxy $curUser
+   *   The current Drupal user.
+   */
+  public function __construct(ClientInterface $http_client,
+                              ConfigFactory $config = NULL,
+                              LoggerChannelFactoryInterface $logger_factory,
+                              MailManager $mailmgr,
+                              AccountProxy $curUser) {
+    $this->httpClient = $http_client;
+    $this->config = $config->get('stanford_earth_r25.adminsettings');
+    $this->logger = $logger_factory->get('system');
+    $this->mailManager = $mailmgr;
+    $this->currentUser = $curUser;
+    $this->settings = $config->get('system.site');
+  }
+
+  /**
+   * Email an R25 error message to the site admin.
+   *
+   * @param array $status
+   *   The request status including an error message.
+   */
+  private function emailErrorToAdmin(array $status) {
+    /*
+    $mod = 'stanford_earth_r25';
+    $key = 'r25_error';
+    $lang = $this->currentUser->getPreferredLangcode();
+    $to = $this->settings->get('mail');
+    $send = TRUE;
+    $result = $this->mailManager->mail($mod, $key, $to, $lang, $status,
+    NULL, $send);
+    if ($result['result'] !== TRUE) {
+    $this->logger->error($status['message']);
+    }
+     */
+  }
+
+  /**
+   * Queries the R25 API and returns response data.
+   *
+   * @param string $command
+   *   The R25 API command.
+   * @param string $post_data
+   *   XML data to be posted to the R25 call.
+   * @param string $id
+   *   An R25 event id.
+   *
+   * @return array
+   *   An array of results data.
+   *
+   * @throws \GuzzleHttp\Exception\GuzzleException
+   */
+  public function r25_api_call(string $command = NULL, string $post_data = NULL, string $id = NULL) {
+    $api_result = [
+      'status' => [
+        'status' => FALSE
+      ],
+      'output' => [],
+    ];
+    try {
+        // get our R25 credential from config
+        $credential = $this->config->get('client_credential');
+        // See the collegenet API documentation for more information: https://knowledge25.collegenet.com/display/WSW/WebServices+API
+        // get the base url for the organization's 25Live back-end
+        $url = $this->config->get('base_url');
+        // add the 25Live admin credential to the url and force it to https
+        $url = 'https://' . $credential . '@' . substr($url, (strpos($url, '://') + 3));
+        // figure out which 25Live webservices API command corresponds to what we want to do
+        switch ($command) {
+          case 'reserve':
+            $xml_command = 'events.xml';
+            break;
+          case 'roominfo':
+            $xml_command = 'space.xml?space_id=' . $post_data;
+            break;
+          case 'roomphoto':
+            $xml_command = 'image?image_id=' . $post_data;
+            break;
+          case 'feed':
+            $xml_command = 'rm_reservations.xml?include=related&' . $post_data;
+            break;
+          case 'delete':
+            $xml_command = 'event.xml?event_id=' . $post_data;
+            break;
+          case 'acctinfo':
+            $xml_command = 'contact.xml?current=T';
+            break;
+          case 'secgroup':
+            $xml_command = 'secgroups.xml?group_name=' . $post_data;
+            break;
+          case 'r25users':
+            $xml_command = 'r25users.xml?security_group_id=' . $post_data;
+            break;
+          case 'evatrb':
+            $xml_command = 'evatrb.xml?attribute_id=' . $post_data;
+            break;
+          case 'event-get':
+            $xml_command = 'event.xml?event_id=' . $post_data;
+            break;
+          case 'event-put':
+            $xml_command = 'event.xml?event_id=' . $id;
+            break;
+          case 'billing-get':
+            $xml_command = 'event.xml?event_id=' . $post_data . '&include=billing+history';
+            break;
+          case 'billing-put':
+            $xml_command = 'event.xml?event_id=' . $id . '&include=billing+history';
+            break;
+          case 'download':
+            $xml_command = 'rm_reservations.xml?' . $post_data . '&include=pending+text+attributes';
+            break;
+         default:
+            $xml_command = 'null.xml';
+        }
+      // add the webservices command to the url
+      $url = rtrim($url, '/') . '/' . $xml_command;
+
+      // depending on what we're doing, the HTTP method will be GET, POST, or DELETE
+      $method = 'GET';
+      $options = [];
+      if ($command == 'reserve') {
+        // $post_data contains the XML for a reservation request
+        $method = 'POST';
+        $options['data'] = $post_data;
+      }
+      else {
+        // $post_data contains the XML to update an event or its billing group id
+        if ($command == 'billing-put' || $command == 'event-put') {
+          $method = 'PUT';
+          $options['data'] = $post_data;
+        }
+        else {
+          if ($command == 'delete') {
+            $method = 'DELETE';
+          }
+        }
+      }
+      $options['method'] = $method;
+      $options['headers'] = array('Content-Type' => 'text/xml; charset=UTF-8');
+      $result = $this->httpClient->request($method,
+        $url,
+        $options);
+
+      // if the request returns an error, report it
+      if ($result->getStatusCode() != 200) {
+        $errmsg = 'HTTP Error response from R25 API: ' . $result->getReasonPhrase();
+        $this->logger->error($errmsg);
+        $api_result['status']['message'] = $errmsg;
+        //$this->emailErrorToAdmin($status);
+        return $api_result;
+      }
+
+      // get the response contents
+      $xmlbody = $result->getBody()->getContents();
+
+      // if getting a room photo, the response body will contain JPEG data instead of XML
+      if ($command == 'roomphoto') {
+        if (empty($xmlbody)) {
+          $api_result['status']['message'] = 'R25 image output is empty.';
+          $this->logger->error($api_result['status']['message']);
+        }
+        else {
+          $api_result['output'] = $xmlbody;
+          $api_result['status']['status'] = TRUE;
+        }
+        return $api_result;
+      }
+
+      // if we have XML output, parse it and create an array to return
+      $vals = [];
+      $index = [];
+      $p = xml_parser_create();
+      xml_parse_into_struct($p, $xmlbody, $vals, $index);
+      xml_parser_free($p);
+
+      // for the null command, we just want a non-error response
+      if ($xml_command == 'null.xml') {
+        if (!empty($vals[0]['tag']) &&
+            $vals[0]['tag'] === 'R25:NULL') {
+              $api_result['status']['status'] = TRUE;
+        }
+        return $api_result;
+      }
+
+      // we can now return the XML values
+      $api_result['output'] = [
+        'vals' => $vals,
+        'index' => $index
+      ];
+
+      // Finally if running commands event-get or billing-get, also
+      // return the raw XML to be modified.
+      if ($command == 'event-get' || $command == 'billing-get') {
+        $api_result['raw-xml'] = $xmlbody;
+      }
+
+      // Return a true status
+      $api_result['status']['status'] = TRUE;
+      return $api_result;
+    }
+    catch (\Exception $e) {
+      $errmsg = 'Error response from R25 API Service: ' . $e->getMessage();
+      $this->logger->error($errmsg);
+      $api_result['status']['message'] = $errmsg;
+      return $api_result;
+    }
+  }
+
+}
