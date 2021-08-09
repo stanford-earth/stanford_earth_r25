176fc2dde3ee0a6d4d49f933c499d59e40f90d57
Kassandra Sharp <ksharp@stanford.edu>
2021-08-08 23:15:28 -0700

    EARTH-0000 Add config entity files

---
+++ src/StanfordEarthR25Interface.php
@@ -0,0 +1,10 @@
+namespace Drupal\stanford_earth_r25;
+
+use Drupal\Core\Config\Entity\ConfigEntityInterface;
+
+/**
+* Provides an interface defining an Stanford Earth R25 Room entity.
+*/
+interface StanfordEarthR25Interface extends ConfigEntityInterface {
+// Add get/set methods for your configuration properties here.
+}
